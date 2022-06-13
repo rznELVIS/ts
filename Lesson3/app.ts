bf95ce632289ee1bcd5a9e8e663828f6d0ded9dc
Дмитрий Демидов <dmitriidemidov@MacBook-Air-Dmitrij-2.local>
2022-06-13 17:18:43 +0300

    Missed files are added

--- Lesson3/app.ts
+++ Lesson3/app.ts
@@ -7,8 +7,13 @@ class User {
 }
 
 let hello: string = "hello world";
-hello = 23;
 
 const tom : User = new User("Том");
+
+let big : bigint = 1000n;
+
+let d : number = 1;
+
 const header = this.document.getElementById("header");
-header.innerHTML = "Привет!! " + tom.name + hello;
+header.innerHTML = `Привет!! " + ${tom.name} ${hello}, number ${d}, bigint: ${big}`;
+
